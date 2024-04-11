dc8ce86dd4efc3320bc6582897f87d42db7b6d5c
nischal <shakyanischal1111@gmail.com>
2024-04-11 23:26:44 +0545

    mvc

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require ("express")
 const dotenv = require('dotenv')
 const app = express();
+const userRoutes = require('./routes/userRoutes')
 
 dotenv.config();
 require('./config/db')
@@ -8,6 +9,9 @@ require('./config/db')
 app.get("/", (req, res)=>{
     res.send("api started")
 })
+
+app.use('/api/user', userRoutes)
+
 let PORT = process.env.PORT
 app.listen(PORT, ()=>{
     console.log(`Server Started at Port ${PORT}`);
---
+++ routes/userRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express')
+
+const router = express.Router()
+
+router.post('/')
+// router.post('/login', authUser)
+
+module.exports = router
